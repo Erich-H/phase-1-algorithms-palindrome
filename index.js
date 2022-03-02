e67f340b31e3556efe968136cbdc88879b60fac5
Erich <eheiseler@gmail.com>
2022-03-02 18:24:30 -0500

    commit tree 6f62b258f8da31d7bc46ae083cf283c0916b5408

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let backwards = reverseWord(word)
+  if(word === backwards) {
+    return true
+  }
+  else
+    return false
+
+}
+
+function reverseWord(string) {
+  if(string === "") {
+    return ""
+  }
+  else
+    return reverseWord(string.substr(1)) + string.charAt(0)
 }
 
 /* 
   Add your pseudocode here
+  create a function that 
+  take a string and reverse it using recursion
+  then call that function isPalindrome and check if the word passed in is equal to the word passed in but reversed
 */
 
 /*
   Add written explanation of your solution here
+  I created a function that took a string and checked if it was empty if not it uses recursion to reverse the word
+  then it gets called in the ispalindrome function to check whether the word is equal to itself backwards
 */
 
 // You can run `node index.js` to view these console logs
